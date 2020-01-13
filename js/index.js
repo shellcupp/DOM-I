04c3210c7c81149188673de03c8cc0605f2cecf2
Michelle Cupp <shellcupp@gmail.com>
2020-01-13 15:09:50 -0600

    getting somewhere

--- js/index.js
+++ js/index.js
@@ -40,3 +40,76 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// "nav": {
+//   "nav-item-1": "Services",
+//   "nav-item-2": "Product",
+//   "nav-item-3": "Vision",
+//   "nav-item-4": "Features",
+//   "nav-item-5": "About",
+//   "nav-item-6": "Contact",
+//   "img-src": "img/logo.png"
+// },
+
+const nav = document.querySelector('nav');
+let navAnchors = document.querySelectorAll('a');
+navAnchors[0].textContent = 'Services';
+navAnchors[1].textContent = 'Product';
+navAnchors[2].textContent = 'Vision';
+navAnchors[3].textContent = 'Features';
+navAnchors[4].textContent = 'About';
+navAnchors[5].textContent = 'Contact';
+
+// "cta": {
+//   "h1": "DOM Is Awesome",
+//   "button": "Get Started",
+//   "img-src": "img/header-img.png"
+// },
+
+const heading = document.querySelector('h1');
+heading.textContent = 'DOM Is Awesome';
+
+const buttontext = document.querySelector('button');
+buttontext.textContent = 'Get Started';
+
+const headingimg = document.querySelector('#cta-img');
+headingimg.src='img/header-img.png'
+
+// "main-content": {
+//   "features-h4":"Features",
+//   ""features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.
+//   "about-h4":"About",
+//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "middle-img-src": "img/mid-page-accent.jpg",
+//   "services-h4":"Services",
+//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "product-h4":"Product",
+//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "vision-h4":"Vision",
+//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// },
+let h4s = document.querySelectorAll('h4');
+h4s[0].textContent = 'FEATURES';
+h4s[1].textContent = 'ABOUT';
+h4s[2].textContent = 'SERVICES';
+h4s[3].textContent = 'PRODUCT';
+h4s[4].textContent = 'VISION';
+h4s[5].textContent = 'CONTACT';
+
+let ps = document.querySelectorAll('p');
+ps[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[1].textContent = 'About-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+ps[5].textContent = '123 Way 456 Street Somewhere, USA';
+ps[6].textContent = '1 (888) 888-8888';
+ps[7].textContent = 'sales@greatidea.io';
+ps[8].textContent = 'Copyright Great Idea! 2018';
+
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// },
